ec8bfa11e20ebcce5236d3f9e71cd3e251654604
Manikantha M <manikantha.m@neutrinos.co>
2024-02-19 18:25:09 +0530

    update  urls and cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-app';
+  appLanguage = 'en';
+  // srcURL = 'http://localhost:4300/motor-insurance/individual-products';
+  srcURL = 'https://b2c-sit.alrajhitakaful.com/web/motor-insurance/individual-products';
+
+  inputData = {
+    isLoggedIn:true,
+    customerType:1,
+    appLanguage:'en',
+    additionalData:{},
+    eventType:"INPUT"
+};
+guestAppLangChange(event:any){
+  console.log(event, 'change in guest language')
 }
+
+getGuestWindowData(event:any){
+  console.log(event, 'received output')
+}
+}
+
+
